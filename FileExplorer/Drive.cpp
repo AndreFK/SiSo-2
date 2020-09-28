ed37e603448968b5d9ea0d73d002af9853b047cd
Andre <andrefigueroak@unitec.edu>
2020-09-28 16:11:09 -0600

    Finished

---
+++ FileExplorer/Drive.cpp
@@ -0,0 +1,105 @@
+﻿#include "stdafx.h"
+#include "Drive.h"
+
+Drive::Drive()
+{
+	_numberOfDrive = 0;
+}
+
+Drive::~Drive()
+{
+	delete(driveLetter);
+	delete(volumeName);
+	delete(driveType);
+}
+
+int Drive::getNumberOfDrive()
+{
+	return _numberOfDrive;
+}
+
+int Drive::getIcon(int i)
+{
+	return icon[i];
+}
+
+TCHAR* Drive::getDriveLetter(int i)
+{
+	return driveLetter[i];
+}
+TCHAR* Drive::getVolumeLabel(int i)
+{
+	return volumeName[i];
+}
+TCHAR* Drive::getDriveType(int i)
+{
+	return driveType[i];
+}
+
+void Drive::getSystemDrives()
+{
+	TCHAR str[STRING_SIZE];
+	long temp = GetLogicalDriveStrings(STRING_SIZE, str);
+	int i = 0;
+
+	while (!(str[i] == '\0' && str[i + 1] == '\0'))
+	{
+		if (str[i] == '\0')
+			_numberOfDrive++;
+		i++;
+	}
+
+	_numberOfDrive++;
+	driveLetter = new TCHAR*[_numberOfDrive];
+	volumeName = new TCHAR*[_numberOfDrive];
+	driveType = new TCHAR*[_numberOfDrive];
+	icon = new int[_numberOfDrive];
+	for (int i = 0; i < _numberOfDrive; i++)
+	{
+		driveLetter[i] = new TCHAR[STRING_SIZE];
+		volumeName[i] = new TCHAR[STRING_SIZE];
+		driveType[i] = new TCHAR[STRING_SIZE];
+	}
+	
+	int index = 0, k;
+	for (int j = 0; j < _numberOfDrive; j++)
+	{
+		k = 0;
+		while (str[index] != 0)
+		{
+			driveLetter[j][k++] = str[index++];
+		}
+		driveLetter[j][k] = '\0';
+		index++;
+	}
+
+	int nType;
+	for (int i = 0; i < _numberOfDrive; ++i)
+	{
+		StrCpy(str, L"");
+		nType = GetDriveType(driveLetter[i]);
+		switch (nType)
+		{
+		case DRIVE_FIXED:
+			StrCpy(driveType[i], L"Local Disk");
+			break;
+		case DRIVE_REMOVABLE:
+			StrCpy(driveType[i], L"Removable Drive");
+			break;
+		case DRIVE_REMOTE:
+			StrCpy(driveType[i], L"Network Drive");
+			break;
+		default:
+			break;
+		}
+		//GetVolumeInformation(driveLetter[i], str, STRING_SIZE, NULL, NULL, NULL, NULL, 0);
+		StrCpy(volumeName[i], str);
+		
+		if (!StrCmp(volumeName[i], L"DATA"))
+			icon[i] = IDI_DATA;
+		else if (!StrCmp(volumeName[i], L"OS"))
+			icon[i] = IDI_OS;
+
+		StrCat(volumeName[i], driveLetter[i]);
+	}
+}
---
+++ FileExplorer/Drive.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <windows.h>
+#include <tchar.h>
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+
+#define IDI_OS 135
+#define IDI_DATA 136
+
+#define STRING_SIZE 256
+class Drive
+{	
+public:
+	Drive();
+	~Drive();
+	int _numberOfDrive;
+	TCHAR** driveLetter;
+	TCHAR** volumeName;
+	TCHAR** driveType;
+	int *icon;
+
+	int getNumberOfDrive();
+	int getIcon(int);
+	TCHAR* getDriveLetter(int);
+	TCHAR* getVolumeLabel(int);
+	TCHAR* getDriveType(int);
+
+	void getSystemDrives();
+};
+
